e2f0f3859c29136b2c9c9c4f22ba01e06296edcf
Megan Null <megan.sorenson@ucdenver.edu>
2021-06-10 08:31:43 -0600

    Update Run_RAREsim.R

--- Example/Run_RAREsim.R
+++ Example/Run_RAREsim.R
@@ -33,6 +33,12 @@ bin_estimates <- expected_variants(Total_num_var = exp_var, mac_bin_prop = bin_p
 count <- read.table(count_file, header=F)
 ToPrune <- prune_variants(MAC = count, expected = bin_estimates)
 
-### write file with variants to prune
+### write file with variants to prune by removing
 prune_file <- paste0(substr(count_file, 1, nchar(count_file)-10),  '_ToPrune.txt')
 write.table(ToPrune$ToRemove, prune_file, row.names=F, quote=F)
+
+### Write file with the variants to prune a subset of alternate alleles
+if(is.null(ToPrune$ToChange) == FALSE){
+subset_file <- paste0(substr(count_file, 1, nchar(count_file)-10),  '.SubsetAA.txt')
+write.table(ToPrune$ToChange, subset_file, row.names=F, quote=F)
+}
