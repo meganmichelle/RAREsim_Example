eb91add26a607bd045cffd87b7832364544e9751
Megan Sorenson <megansorenson@Megans-MacBook-Pro-2.local>
2021-08-12 10:33:09 -0600

    Update Example

--- Example/Example_code.sh
+++ Example/Example_code.sh
@@ -21,41 +21,18 @@ NE=17469
 -Ne $NE \
 -no_gens_output 
 
+### Remove unnecessary files produced by HAPGEN2
 rm $Output.cases.*
 
-### create the MAC file
-zcat $Output.controls.haps.gz | awk '{sum=0; for(i=1; i<=NF; i++) sum += $i; print sum}' > $Output.count.txt
 
-### delete the variants with 0 alternate alleles (no variation - monomorphic variants)
-### must be in directory with R code
+### Calculate the number of variants per MAC bin with the RAREsim R package
 
-### replace the first line of the R script with the necessary file location
-count="count_file <- '$Output.count.txt'"
-sed -i "1c${count}" Delete_Monomorphic.R 
-
-### run the Rscript to create the .rem.sed file
-Rscript Delete_Monomorphic.R 
-
-#### unzip the haplotype file 
-gunzip $Output.controls.haps.gz
-
-
-### remove the monomorphic variants from the haplotype file
-sed -i -f $Output.rem.sed $Output.controls.haps 
-
-### remove the legend file header (so line number directly corresponds with the haplotype file)
-sed -i '1d' $Output.legend
-
-### remove the monomorphic variants from the legend file
-sed -i -f $Output.rem.sed $Output.legend 
-
-### create an updated MAC file for pruning
-cat $Output.controls.haps | awk '{sum=0; for(i=1; i<=NF; i++) sum += $i; print sum}' > $Output.count.txt
-
-### relace the first four lines of the Run_RAREsim.R script with the necessary variables
+### replace the first four lines of the Run_RAREsim.R script with the necessary variables
 Nsim2="nsim <- $Nsim"
+
 pop2="pop <- '$pop'"
 reg_size="reg_size <- $Reg_size_kb\ "
+
 sed -i "1c${Nsim2}" Run_RAREsim.R
 sed -i "2c${pop2}" Run_RAREsim.R
 sed -i "3c${count}" Run_RAREsim.R
@@ -66,78 +43,20 @@ sed -i "4c${reg_size}" Run_RAREsim.R
 Rscript Run_RAREsim.R
 
 
-### Implement pruning:
+### Convert the haplotype file into a sparse matrix
 
-#### Check if the subset AA file was written within pruning
-
-FILE=$Output.SubsetAA.txt
-if test -f "$FILE"; then
-    echo "$FILE exists."
-    ### prune commands!
+python convert.py \
+    -i $Output.controls.haps.gz \
+    -o $Output.controls.haps.gz.sm
 
 
-awk '{print $1}' $Output.SubsetAA.txt > ChangeLines.txt
-
-## Delete  the header in ChangeLines so we can just loop through the lines
-
-sed -i '1d' ChangeLines.txt
-
-##### Extract each line from the haplotype file that needs a subset of variants removed
-
-while read ln; do
-
-ln1=$(( $ln + 1))
-sed -n "${ln}p;${ln1}q" $Output.controls.haps >> VariantsToEdit.txt
-
-done <ChangeLines.txt 
-
-### 3. Use the RAREsim package to take the ToChange file and edit the lines (variants) - based on the ToPrune file 
-
-add1="original_variants_file<-'VariantsToEdit.txt'"
-add2="new_mac_file<-'$Output.SubsetAA.txt'"
-add3="con1<-'$Output.controls.haps'"
-add4="con2<-'$Output.controls.updated.haps'"
-
-sed -i "1c${add1}" Prune_subset_of_AA.R
-sed -i "2c${add2}" Prune_subset_of_AA.R
-sed -i "3c${add3}" Prune_subset_of_AA.R
-sed -i "4c${add4}" Prune_subset_of_AA.R
-
-Rscript Prune_subset_of_AA.R
-
-## Replace the new haplotype file with the old one
-rm $Output.controls.haps
-cp $Output.controls.updated.haps $Output.controls.haps
-rm $Output.controls.updated.haps
-
-rm VariantsToEdit.txt
-
-
-fi
-
-## completely remove other pruned variants
-
-prune="ToDel_file <- '$Output\_ToPrune.txt'"
-sed -i "1c${prune}" Prune_Variants.R 
-
-### run the Rscript to create the List2Delete.sed file
-
-Rscript Prune_Variants.R
-
-### remove the pruned variants from the haplotype
-sed -i -f $Output\_List2delete.sed $Output.controls.haps
-
-## re-zip the haplotype and genotype files
-gzip $Output.controls.haps
-
-
-### remove the pruned variants from the legend file
-sed -i -f $Output\_List2delete.sed $Output.legend 
-
-### add the header back to the legend file
-sed -i " 1s|^|rs position X0 X1\n| " $Output.legend 
-
-
+### Prune the haplotype to match what is expected
+$ python sim.py \
+    -m $Output.controls.haps.gz.sm \
+    -b lib/raresim/test/data/Expected_variants_per_bin_80k.txt \
+    -l $Output.legend \
+    -L $Output.new.legend \
+    -H $Output.new.hap.gz
 
 
 
--- Example/Run_RAREsim.R
+++ Example/Run_RAREsim.R
@@ -29,16 +29,7 @@ bin_props <- afs(mac_bins = mac, pop = pop)
 ### expected number of variants per bin
 bin_estimates <- expected_variants(Total_num_var = exp_var, mac_bin_prop = bin_props)
 
-### prune variants function
-count <- read.table(count_file, header=F)
-ToPrune <- prune_variants(MAC = count, expected = bin_estimates)
-
 ### write file with variants to prune by removing
-prune_file <- paste0(substr(count_file, 1, nchar(count_file)-10),  '_ToPrune.txt')
-write.table(ToPrune$ToRemove, prune_file, row.names=F, quote=F)
+write.table(bin_estimates, 'MAC_bin_estimates.txt', row.names=F, quote=F,
+            sep = '\t')
 
-### Write file with the variants to prune a subset of alternate alleles
-if(is.null(ToPrune$ToChange) == FALSE){
-subset_file <- paste0(substr(count_file, 1, nchar(count_file)-10),  '.SubsetAA.txt')
-write.table(ToPrune$ToChange, subset_file, row.names=F, quote=F)
-}
